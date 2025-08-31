4370d86123c6a352c4d3cff5bb22793e0b943f85
May <120320033+Moshika-May@users.noreply.github.com>
2025-08-31 07:39:28 +0000

    assignment_work_L02(A02)_L02(A03)

---
+++ Assignments/math_op.py
@@ -0,0 +1,22 @@
+# 6810742293 กษิดิ์เดช มหานิล
+# Input values
+first_num = int(input('Enter first number: '))
+second_num = int(input('Enter secnod number: '))
+
+# Calculation values
+calculation_a = first_num + second_num
+calculation_b = first_num - second_num
+calculation_c = first_num * second_num
+calculation_d = first_num / second_num
+calculation_e = first_num // second_num
+calculation_f = first_num % second_num
+calculation_g = first_num ** second_num
+
+# print section
+print(f'{first_num} + {second_num} = {calculation_a}',
+      f'{first_num} - {second_num} = {calculation_b}',
+      f'{first_num} * {second_num} = {calculation_c}',
+      f'{first_num} / {second_num} = {calculation_d}',
+      f'{first_num} // {second_num} = {calculation_e}',
+      f'{first_num} % {second_num} = {calculation_f}',
+      f'{first_num} ** {second_num} = {calculation_g}', sep=('\n'))
