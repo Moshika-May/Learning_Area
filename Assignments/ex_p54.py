1332425bc57410b0e63c60b6ab08c2b62cb37914
SakolpatRungrit <ayasenakiri@gmail.com>
2025-09-02 09:51:47 +0000

    Fixed

--- Assignments/ex_p54.py
+++ Assignments/ex_p54.py
@@ -10,18 +10,19 @@ power_unit = int(input('Enter the energy consumption (kilowatt-hours): '))
 # calculate electricity cost
 if power_unit >= 0:
     if power_unit <= 150:
-        unit_price = 3.2484
+        electicity_cost = power_unit * 3.2484
     elif power_unit <= 400:
-        unit_price = 4.2218
+        electicity_cost = (150 * 3.2484) + ((power_unit - 150) * 4.2218)
     else:
-        unit_price = 4.4217
-    electicity_cost = power_unit * unit_price
+        electicity_cost = (150 * 3.2484) + (250 * 4.2218) + ((power_unit - 400) * 4.4217)
+
+    
     vat_cost = (SERVICE_CHARGE + electicity_cost) * VAT
     total_cost = SERVICE_CHARGE + electicity_cost + vat_cost
 
     print(f'Electricity cost = {electicity_cost:.2f} Baht',
-        f'Service charge = {SERVICE_CHARGE:.2f} Baht',
-        f'VAT (7 Percent) = {vat_cost:.2f} Baht',
-        f'Total cost = {total_cost:.2f} Baht', sep='\n')
+          f'Service charge = {SERVICE_CHARGE:.2f} Baht',
+          f'VAT (7 Percent) = {vat_cost:.2f} Baht',
+          f'Total cost = {total_cost:.2f} Baht', sep='\n')
 else:
     print('Invalid input')
