4cc56b712e191d6a8ac43e24623ee44c778af499
May <120320033+Moshika-May@users.noreply.github.com>
2025-09-02 08:40:47 +0000

    Refactor pricing calculation to improve clarity and output format

--- Assignments/ex_p52.py
+++ Assignments/ex_p52.py
@@ -1,16 +1,26 @@
 # Kasidej Mahanin 6810742293
+
+# Constants
 PACKAGE_PRICE = 99.00
 
+# Input number of packages
 number_of_packages = int(input('Number of packages purchased: '))
-normal_price = number_of_packages * PACKAGE_PRICE
-if number_of_packages >= 0:
-    if number_of_packages >= 10 and number_of_packages <= 19:
+discount = 0.00
+# Calculate total price with discount
+if number_of_packages >= 10 and number_of_packages <= 19:
         discount = 0.10
-    elif number_of_packages >= 20 and number_of_packages <= 49:
+elif number_of_packages >= 20 and number_of_packages <= 49:
         discount = 0.20
-    elif number_of_packages >= 50 and number_of_packages <= 99:
+elif number_of_packages >= 50 and number_of_packages <= 99:
         discount = 0.30
-    elif number_of_packages >= 100:
+elif number_of_packages >= 100:
         discount = 0.40
-    total_price = number_of_packages * PACKAGE_PRICE * (1 - discount)
-    print(f'Total amount due: {total_price:.2f} Baht')
+total_price = number_of_packages * PACKAGE_PRICE * (1 - discount)
+normal_price = number_of_packages * PACKAGE_PRICE
+discount_price = normal_price * discount
+
+# Print the result
+print(f'Normal amount: {normal_price:,.2f} Baht',
+      f'Discount amount: {discount_price:,.2f} Baht',
+      f'Total amount: {total_price:,.2f} Baht', sep='\n'
+)
