fcfe76ccb048bdd46861322416a02ae267a3d4cf
May <120320033+Moshika-May@users.noreply.github.com>
2025-09-03 06:46:17 +0000

    Refactor variable declarations and improve input prompts for clarity

--- Random_Task/random_program_c_1.c
+++ Random_Task/random_program_c_1.c
@@ -1,6 +1,42 @@
 #include <stdio.h>
 
 int main() {
-    float f1, f2, f3;
+    float f1;
+    int f2;
+    char f3[20];
+    printf("Enter first float: ");
+    scanf("%f", &f1);
+    printf("Enter second int: ");
+    scanf("%d", &f2);
+    printf("Enter third char: ");
+    scanf("%s", &f3);
+    
+    printf("===========================\n");
+    printf("Float: %.2f\n", f1);
+    printf("Int: %d\n", f2);
+    printf("Char: %s\n", f3);
     return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
