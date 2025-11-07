8a8b2b664f0f165ed8e01075a2d4b9bdd3f454bb
Moshika-May <Color7338@gmail.com>
2025-11-07 19:14:12 +0700

    Clear_Assignment_Week

--- Assignments/nearest_integer.py
+++ Assignments/nearest_integer.py
@@ -1,16 +1,17 @@
-import math
+# 6810742293 Kasidej Mahanin
+
+from math import floor, ceil
 
 def checking(x):
-    x_floor = math.floor(x)
-    x_ceil = math.ceil(x)
-    if x > x_floor:
+    x_floor = floor(x)
+    x_ceil = ceil(x)
+
+    if x - x_floor == 0.5:
+        print(f'{x_floor} - {x} - {x_ceil}')
+    elif x - x_floor > 0.5:
         print(f'{x} -> {x_ceil}')
-    elif x < x_floor:
-        print(f'{x} <- {x_ceil}')
-    elif x > x_ceil:
-        print(f'{x} -> {x_floor}')
-    elif x < x_ceil:
-        print(f'{x} <- {x_floor}')
+    else:
+        print(f'{x_floor} <- {x}')
 
 def main():
     number = float(input('Type a number: '))
